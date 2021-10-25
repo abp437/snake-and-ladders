d7ab8274af4a4839f899c84f59cc54a889add3f7
abp437 <abp437@gmail.com>
2021-10-25 09:49:35 +0530

    Add Constant & EventListener classes

--- index.js
+++ index.js
@@ -1,163 +1,189 @@
-const availableBoards = {
-  easy: {
-    uiSpecs: {
-      rowSize: 5,
-      rowCount: 5,
-      nodeSizeInPx: 90,
-      verticalOffsetInPx: 36,
-      horizontalOffsetInPx: 40,
+class Constant {
+  static availableBoards = {
+    easy: {
+      uiSpecs: {
+        rowSize: 5,
+        rowCount: 5,
+        nodeSizeInPx: 90,
+        verticalOffsetInPx: 36,
+        horizontalOffsetInPx: 40,
+      },
+      snakesList: [
+        { head: 6, tail: 4 },
+        { head: 20, tail: 9 },
+        { head: 22, tail: 17 },
+      ],
+      laddersList: [
+        { bottom: 2, top: 8 },
+        { bottom: 10, top: 18 },
+        { bottom: 15, top: 17 },
+      ],
     },
-    snakesList: [
-      { head: 6, tail: 4 },
-      { head: 20, tail: 9 },
-      { head: 22, tail: 17 },
-    ],
-    laddersList: [
-      { bottom: 2, top: 8 },
-      { bottom: 10, top: 18 },
-      { bottom: 15, top: 17 },
-    ],
-  },
 
-  medium: {
-    uiSpecs: {
-      rowSize: 10,
-      rowCount: 10,
-      nodeSizeInPx: 61,
-      verticalOffsetInPx: 11,
-      horizontalOffsetInPx: 19,
+    medium: {
+      uiSpecs: {
+        rowSize: 10,
+        rowCount: 10,
+        nodeSizeInPx: 61,
+        verticalOffsetInPx: 11,
+        horizontalOffsetInPx: 19,
+      },
+      snakesList: [
+        { head: 38, tail: 20 },
+        { head: 51, tail: 10 },
+        { head: 76, tail: 54 },
+        { head: 91, tail: 73 },
+        { head: 97, tail: 61 },
+      ],
+      laddersList: [
+        { bottom: 5, top: 58 },
+        { bottom: 14, top: 49 },
+        { bottom: 53, top: 72 },
+        { bottom: 64, top: 83 },
+      ],
     },
-    snakesList: [
-      { head: 38, tail: 20 },
-      { head: 51, tail: 10 },
-      { head: 76, tail: 54 },
-      { head: 91, tail: 73 },
-      { head: 97, tail: 61 },
-    ],
-    laddersList: [
-      { bottom: 5, top: 58 },
-      { bottom: 14, top: 49 },
-      { bottom: 53, top: 72 },
-      { bottom: 64, top: 83 },
-    ],
-  },
 
-  difficult: {
-    uiSpecs: {
-      rowSize: 10,
-      rowCount: 10,
-      nodeSizeInPx: 59,
-      verticalOffsetInPx: 13,
-      horizontalOffsetInPx: 23,
+    difficult: {
+      uiSpecs: {
+        rowSize: 10,
+        rowCount: 10,
+        nodeSizeInPx: 59,
+        verticalOffsetInPx: 13,
+        horizontalOffsetInPx: 23,
+      },
+      snakesList: [
+        { head: 17, tail: 7 },
+        { head: 54, tail: 34 },
+        { head: 62, tail: 19 },
+        { head: 64, tail: 60 },
+        { head: 87, tail: 36 },
+        { head: 93, tail: 73 },
+        { head: 95, tail: 75 },
+        { head: 98, tail: 79 },
+      ],
+      laddersList: [
+        { bottom: 1, top: 38 },
+        { bottom: 4, top: 14 },
+        { bottom: 9, top: 31 },
+        { bottom: 21, top: 42 },
+        { bottom: 28, top: 84 },
+        { bottom: 51, top: 67 },
+        { bottom: 72, top: 91 },
+        { bottom: 80, top: 99 },
+      ],
     },
-    snakesList: [
-      { head: 17, tail: 7 },
-      { head: 54, tail: 34 },
-      { head: 62, tail: 19 },
-      { head: 64, tail: 60 },
-      { head: 87, tail: 36 },
-      { head: 93, tail: 73 },
-      { head: 95, tail: 75 },
-      { head: 98, tail: 79 },
-    ],
-    laddersList: [
-      { bottom: 1, top: 38 },
-      { bottom: 4, top: 14 },
-      { bottom: 9, top: 31 },
-      { bottom: 21, top: 42 },
-      { bottom: 28, top: 84 },
-      { bottom: 51, top: 67 },
-      { bottom: 72, top: 91 },
-      { bottom: 80, top: 99 },
-    ],
-  },
 
-  classic: {
-    uiSpecs: {
-      rowSize: 10,
-      rowCount: 10,
-      nodeSizeInPx: 61,
-      verticalOffsetInPx: 11,
-      horizontalOffsetInPx: 19,
+    classic: {
+      uiSpecs: {
+        rowSize: 10,
+        rowCount: 10,
+        nodeSizeInPx: 61,
+        verticalOffsetInPx: 11,
+        horizontalOffsetInPx: 19,
+      },
+      snakesList: [
+        { head: 40, tail: 3 },
+        { head: 43, tail: 18 },
+        { head: 27, tail: 5 },
+        { head: 54, tail: 31 },
+        { head: 66, tail: 45 },
+        { head: 76, tail: 58 },
+        { head: 89, tail: 53 },
+        { head: 99, tail: 41 },
+      ],
+      laddersList: [
+        { bottom: 4, top: 25 },
+        { bottom: 13, top: 46 },
+        { bottom: 33, top: 49 },
+        { bottom: 42, top: 63 },
+        { bottom: 50, top: 69 },
+        { bottom: 62, top: 81 },
+        { bottom: 74, top: 92 },
+      ],
     },
-    snakesList: [
-      { head: 40, tail: 3 },
-      { head: 43, tail: 18 },
-      { head: 27, tail: 5 },
-      { head: 54, tail: 31 },
-      { head: 66, tail: 45 },
-      { head: 76, tail: 58 },
-      { head: 89, tail: 53 },
-      { head: 99, tail: 41 },
-    ],
-    laddersList: [
-      { bottom: 4, top: 25 },
-      { bottom: 13, top: 46 },
-      { bottom: 33, top: 49 },
-      { bottom: 42, top: 63 },
-      { bottom: 50, top: 69 },
-      { bottom: 62, top: 81 },
-      { bottom: 74, top: 92 },
-    ],
-  },
 
-  rockets: {
-    uiSpecs: {
-      rowSize: 10,
-      rowCount: 10,
-      nodeSizeInPx: 61,
-      verticalOffsetInPx: 10,
-      horizontalOffsetInPx: 16,
+    rockets: {
+      uiSpecs: {
+        rowSize: 10,
+        rowCount: 10,
+        nodeSizeInPx: 61,
+        verticalOffsetInPx: 10,
+        horizontalOffsetInPx: 16,
+      },
+      snakesList: [
+        { head: 43, tail: 17 },
+        { head: 50, tail: 5 },
+        { head: 56, tail: 8 },
+        { head: 73, tail: 15 },
+        { head: 84, tail: 58 },
+        { head: 87, tail: 49 },
+        { head: 98, tail: 40 },
+      ],
+      laddersList: [
+        { bottom: 2, top: 23 },
+        { bottom: 4, top: 68 },
+        { bottom: 6, top: 45 },
+        { bottom: 20, top: 59 },
+        { bottom: 30, top: 96 },
+        { bottom: 52, top: 72 },
+        { bottom: 57, top: 96 },
+        { bottom: 71, top: 92 },
+      ],
     },
-    snakesList: [
-      { head: 43, tail: 17 },
-      { head: 50, tail: 5 },
-      { head: 56, tail: 8 },
-      { head: 73, tail: 15 },
-      { head: 84, tail: 58 },
-      { head: 87, tail: 49 },
-      { head: 98, tail: 40 },
-    ],
-    laddersList: [
-      { bottom: 2, top: 23 },
-      { bottom: 4, top: 68 },
-      { bottom: 6, top: 45 },
-      { bottom: 20, top: 59 },
-      { bottom: 30, top: 96 },
-      { bottom: 52, top: 72 },
-      { bottom: 57, top: 96 },
-      { bottom: 71, top: 92 },
-    ],
-  },
 
-  // This board has a special case at position 98, it has snake's head at the ladder's top
-  dragons: {
-    uiSpecs: {
-      rowSize: 10,
-      rowCount: 10,
-      nodeSizeInPx: 60,
-      verticalOffsetInPx: 11,
-      horizontalOffsetInPx: 19,
+    // This board has a special case at position 98, it has snake's head at the ladder's top
+    dragons: {
+      uiSpecs: {
+        rowSize: 10,
+        rowCount: 10,
+        nodeSizeInPx: 60,
+        verticalOffsetInPx: 11,
+        horizontalOffsetInPx: 19,
+      },
+      snakesList: [
+        { head: 30, tail: 16 },
+        { head: 37, tail: 3 },
+        { head: 56, tail: 8 },
+        { head: 84, tail: 64 },
+        { head: 87, tail: 31 },
+        { head: 98, tail: 40 },
+      ],
+      laddersList: [
+        { bottom: 2, top: 23 },
+        { bottom: 6, top: 45 },
+        { bottom: 20, top: 59 },
+        { bottom: 52, top: 72 },
+        { bottom: 57, top: 96 },
+        { bottom: 74, top: 94 },
+        { bottom: 78, top: 98 },
+      ],
     },
-    snakesList: [
-      { head: 30, tail: 16 },
-      { head: 37, tail: 3 },
-      { head: 56, tail: 8 },
-      { head: 84, tail: 64 },
-      { head: 87, tail: 31 },
-      { head: 98, tail: 40 },
-    ],
-    laddersList: [
-      { bottom: 2, top: 23 },
-      { bottom: 6, top: 45 },
-      { bottom: 20, top: 59 },
-      { bottom: 52, top: 72 },
-      { bottom: 57, top: 96 },
-      { bottom: 74, top: 94 },
-      { bottom: 78, top: 98 },
-    ],
-  },
-};
+  };
+}
+
+class Util {
+  static spacebarKeyCode = 32;
+
+  static htmlElemFromString(htmlStr) {
+    const parser = new DOMParser();
+    return parser.parseFromString(htmlStr, "text/html").body
+      .firstChild;
+  }
+
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static shuffleArray(arr) {
+    arr.sort(() => Math.random() - 0.5);
+  }
+
+  static isSpacebarPressed(e) {
+    return e.keyCode === this.spacebarKeyCode;
+  }
+
+  static padToTwoDigits = (digit) => (`0${digit}`).slice(-2);
+}
 
 class Dice {
   static getDiceFaceHtml(diceRoll) {
@@ -304,7 +330,7 @@ class Board {
 
   constructor(playerCount, boardName) {
     this.#playerCount = playerCount;
-    this.#boardData = availableBoards[boardName];
+    this.#boardData = Constant.availableBoards[boardName];
     this.#boardName = boardName;
     this.#possiblePlayers = ["beige", "black", "greenyellow", "salmon"];
     this.#initializeBoardImage();
@@ -607,37 +633,15 @@ class Player {
   }
 }
 
-class Util {
-  static spacebarKeyCode = 32;
-
-  static htmlElemFromString(htmlStr) {
-    const parser = new DOMParser();
-    return parser.parseFromString(htmlStr, "text/html").body
-      .firstChild;
+class EventListener {
+  static selectBoard(boardsList, event) {
+    const selectedBoard = document.getElementById("selected-board");
+    boardsList.forEach((elem) => {
+      elem.classList.remove("selected");
+    });
+    selectedBoard.value = event.currentTarget.getAttribute("data-board");
+    event.currentTarget.classList.add("selected");
   }
-
-  static capitalize(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-  }
-
-  static shuffleArray(arr) {
-    arr.sort(() => Math.random() - 0.5);
-  }
-
-  static isSpacebarPressed(e) {
-    return e.keyCode === this.spacebarKeyCode;
-  }
-
-  static padToTwoDigits = (digit) => (`0${digit}`).slice(-2);
-}
-
-function selectBoard(boardsList, event) {
-  const selectedBoard = document.getElementById("selected-board");
-  boardsList.forEach((elem) => {
-    elem.classList.remove("selected");
-  });
-  selectedBoard.value = event.currentTarget.getAttribute("data-board");
-  event.currentTarget.classList.add("selected");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -679,7 +683,7 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 
   boardsList.forEach((elem) => {
-    elem.addEventListener("click", selectBoard.bind(null, boardsList));
+    elem.addEventListener("click", EventListener.selectBoard.bind(null, boardsList));
   });
 
   document.body.onkeyup = (e) => {
